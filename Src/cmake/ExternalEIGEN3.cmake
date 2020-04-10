054f643fbdfc9ba866d59e691fd6e81feb25146d
Juergen Bohl <jbohl@h-quer.de>
2020-04-10 23:36:34 +0200

    cmake

--- Src/cmake/ExternalEIGEN3.cmake
+++ Src/cmake/ExternalEIGEN3.cmake
@@ -3,11 +3,13 @@ include(ExternalProject)
 ExternalProject_Add(
   eigen_ext
   GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+  #GIT_TAG        39142904cc2301628931481e8b331cc2d567e22f  # this is "known-to-work", just in case
   UPDATE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
-  LOG_DOWNLOAD ON
-  LOG_CONFIGURE ON)
+  #LOG_DOWNLOAD ON
+  #LOG_CONFIGURE ON
+  )
 
 ExternalProject_Get_Property(eigen_ext source_dir)
 set(EIGEN3_INCLUDE_DIRS ${source_dir})
