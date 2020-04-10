eb869aa47d974d7c236814219b9015fd4fba3c9e
Juergen Bohl <jbohl@h-quer.de>
2020-04-11 01:38:05 +0200

    cmake

--- Src/cmake/ExternalEIGEN3.cmake
+++ Src/cmake/ExternalEIGEN3.cmake
@@ -2,8 +2,12 @@ include(ExternalProject)
 
 ExternalProject_Add(
   eigen_ext
+  PREFIX "vendor/eigen3"
   GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
   #GIT_TAG        39142904cc2301628931481e8b331cc2d567e22f  # this is "known-to-work", just in case
+  CMAKE_ARGS
+    -DEIGEN_BUILD_PKGCONFIG=OFF
+    -DBUILD_TESTING=OFF
   UPDATE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
