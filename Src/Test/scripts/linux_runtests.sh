6f555f28c85c60bad43e8a4d916bfd77c531dbbe
J. Bohl <jbohl@h-quer.de>
2020-04-14 18:55:53 +0200

    test on linux

--- Src/Test/scripts/linux_runtests.sh
+++ Src/Test/scripts/linux_runtests.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 #EXECUTABLE=~/github/libCZI/Src/CZIcmd/CZIcmd
-EXECUTABLE=~/github/libCZI/Src/CZICmd/CZIcmd
-SRCIMAGES=/home/jbohl/atom2/hdd/2/tmp/TestData/
+#EXECUTABLE=~/github/libCZI/Src/CZICmd/CZIcmd
+EXECUTABLE=~/Dev/New/buildlibCZI/Src/CZICmd/CZIcmd
+#SRCIMAGES=/home/jbohl/atom2/hdd/2/tmp/TestData/
+SRCIMAGES=/mnt/atom2/hdd/2/TestData/
+OUTIMAGES=~/testOut/
 
 if [ -z "$1"] 
 then
-  python runtests.py -s $SRCIMAGES -d /home/jbohl/atom2/hdd/2/tmp/TestOut/ -e $EXECUTABLE -t ./testdata_linux.txt 
+  python runtests.py -s $SRCIMAGES -d $OUTIMAGES -e $EXECUTABLE -t ./testdata_linux.txt 
 else
-  python runtests.py -s $SRCIMAGES -d /home/jbohl/atom2/hdd/2/tmp/TestOut/ -e $EXECUTABLE -t ./testdata_linux.txt --skip $1
+  python runtests.py -s $SRCIMAGES -d $OUTIMAGES -e $EXECUTABLE -t ./testdata_linux.txt --skip $1
 fi
 
